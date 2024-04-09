d2681aa792048d68de1f703ced277a68107afc85
Zieus Jacob <131012988+Terminaltalk@users.noreply.github.com>
2024-04-09 16:02:49 +0200

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: process.env.TOKEN,
+  TOKEN: process.env.TOKEN || "MTEwNTE5MDM4NTkyNTQ5Mjc0OA.Gu52dK.Co2b7XghW6MYT2AB8UbGzbUQTcnEMFHSTTW-6k",
   PREFIX: process.env.PREFIX || "?",
   embed: {
     color: process.env.color || "#cbcbcb",
