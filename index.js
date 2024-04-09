0e4aa48165e68405e39fb93dcbf293cf697976fd
Zieus Jacob <131012988+Terminaltalk@users.noreply.github.com>
2024-04-09 16:00:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const JUGNU = require("./handlers/Client");
 const { TOKEN } = require("./settings/config");
 
@@ -6,4 +5,4 @@ const client = new JUGNU();
 
 module.exports = client;
 
-client.start(TOKEN);
+client.start("MTEwNTE5MDM4NTkyNTQ5Mjc0OA.Gu52dK.Co2b7XghW6MYT2AB8UbGzbUQTcnEMFHSTTW-6k");
